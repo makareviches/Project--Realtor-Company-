3fe9ffa0709e8dacd555156c37ba2301da3a762b
makareviches <makareviches@github.com>
2020-05-20 23:42:31 +0300

    3 queries

--- Queries.sql
+++ Queries.sql
@@ -1,3 +1,22 @@
+--
+-- Choosing from table `Apartments` only that located on South avenue and have 2 bd
+--
+
 SELECT *
 FROM Apartments
 WHERE street_adres='1536 South Ave' AND number_of_bd=2;
+
+--
+-- Choosing from table `Realtors` whose name started with 'J' and omission more than 0.10
+--
+
+SELECT *
+FROM Realtors
+WHERE full_name LIKE 'J%' AND comission>0.10;
+
+--
+-- Choosing from table `Apartments` whose name started with 'J' and omission more than 0.10
+--
+SELECT *
+FROM Apartments
+WHERE number_of_bd=1 AND home_value>500000 and home_value<700000;
