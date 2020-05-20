7a2a91a7764f0e2be4fb52e9b33ebe556e240df9
makareviches <makareviches@github.com>
2020-05-20 23:04:41 +0300

    created db, tables, 1st query

---
+++ Creating table.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE Project;
+USE Project;
+
+CREATE TABLE Realtors (
+  realtor_id int NOT NULL IDENTITY(1,1) PRIMARY KEY, 
+  full_name varchar(255) NOT NULL,
+  comission float, 
+ ); 
+
+
+CREATE TABLE Apartments (
+  apartment_id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+  street_adres VARCHAR(255) NOT NULL,
+  apartment INT NOT NULL,
+  number_of_bd INT NOT NULL,
+  square_footage FLOAT NOT NULL,
+  home_value money NOT NULL,
+  ); 
+
+
+ CREATE TABLE Sales_transactions (
+  sales_transaction_id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+  sale_date DATE NOT NULL,
+  realtor_id INT NOT NULL FOREIGN KEY REFERENCES realtors(realtor_id ), 
+  apartment_id INT NOT NULL FOREIGN KEY REFERENCES apartments(apartment_id),
+  ); 
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
---
+++ Queries.sql
@@ -0,0 +1,3 @@
+SELECT *
+FROM Apartments
+WHERE street_adres='1536 South Ave' AND number_of_bd=2;
